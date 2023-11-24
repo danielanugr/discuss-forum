017a214b2dacd0422d6857be748649b056de7788
Daniel Adhi Nugraha <danielanugr@yahoo.co.id>
2023-11-24 19:46:48 +0800

    add form to create topic, inti home page
    Signed-off-by: Daniel Adhi Nugraha <danielanugr@yahoo.co.id>

---
+++ src/components/topics/topic-create-form.tsx
@@ -0,0 +1,37 @@
+import {
+  Button,
+  Input,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@nextui-org/react';
+import { createTopic } from '@/utils';
+
+export default function TopicCreateForm() {
+  return (
+    <Popover placement='left'>
+      <PopoverTrigger>
+        <Button color='primary'>Create A Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={createTopic}>
+          <div className='flex flex-col gap-4 p-4 w-80'>
+            <h3 className='text-lg'>Create a topic</h3>
+            <Input
+              label='Name'
+              labelPlacement='outside'
+              placeholder='Topic Name'
+            />
+            <Textarea
+              label='Description'
+              labelPlacement='outside'
+              placeholder='Describe your topic'
+            />
+            <Button type='submit'>Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+}
