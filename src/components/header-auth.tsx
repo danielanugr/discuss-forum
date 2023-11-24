561bff22e84bfbc78be46627b2868b8d9912fed1
Daniel Adhi Nugraha <danielanugr@yahoo.co.id>
2023-11-24 19:29:04 +0800

    fix header auth making every page dynamic
    Signed-off-by: Daniel Adhi Nugraha <danielanugr@yahoo.co.id>

---
+++ src/components/header-auth.tsx
@@ -0,0 +1,61 @@
+'use client';
+import Link from 'next/link';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+  Button,
+  Avatar,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@nextui-org/react';
+import { useSession } from 'next-auth/react';
+import * as utils from '@/utils';
+
+export default function HeaderAuth() {
+  const session = useSession();
+
+  let authContent: React.ReactNode;
+  if (session.status === 'loading') {
+    authContent = null;
+  } else if (session.data?.user) {
+    authContent = (
+      <Popover placement='left'>
+        <PopoverTrigger>
+          <Avatar src={session.data.user.image || ''} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className='p-4'>
+            <form action={utils.signOut}>
+              <Button type='submit'>Sign Out</Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
+    );
+  } else {
+    authContent = (
+      <>
+        <NavbarItem>
+          <form action={utils.signIn}>
+            <Button type='submit' color='secondary' variant='bordered'>
+              Sign In
+            </Button>
+          </form>
+        </NavbarItem>
+        <NavbarItem>
+          <form action={utils.signIn}>
+            <Button type='submit' color='primary' variant='flat'>
+              Sign Up
+            </Button>
+          </form>
+        </NavbarItem>
+      </>
+    );
+  }
+
+  return authContent;
+}
